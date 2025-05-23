1cfb68210e869b60a13a74c56098dce34aa30b26
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2025-05-24 03:06:40 +0530

    First commit

---
+++ Ecommerce/EcommerceTests/EcommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceTests.swift
+//  EcommerceTests
+//
+//  Created by Nikhil Dhavale on 24/05/25.
+//
+
+import Testing
+@testable import Ecommerce
+
+struct EcommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Ecommerce/EcommerceUITests/EcommerceUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EcommerceUITestsLaunchTests.swift
+//  EcommerceUITests
+//
+//  Created by Nikhil Dhavale on 24/05/25.
+//
+
+import XCTest
+
+final class EcommerceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
