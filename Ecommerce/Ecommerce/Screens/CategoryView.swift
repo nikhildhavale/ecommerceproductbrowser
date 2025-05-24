6abf19fb8ebe2f565c16e0645467be1404d61e12
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2025-05-24 07:47:33 +0530

    horizontal work in progresss to show product list

--- Ecommerce/Ecommerce/Screens/CategoryView.swift
+++ Ecommerce/Ecommerce/Screens/CategoryView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 // MARK: - CategoryView
 struct CategoryView: View {
+    @ObservedObject var categoryViewModel = CategoryViewModel()
     var body: some View {
         VStack {
-            HorizontalScroller(viewModel: HorizontalScrollerModel())
-            
+            HorizontalScroller(viewModel: categoryViewModel.model)
+            ///products?price_min=100&price_max=500&categoryId=3
         }
     }
+}
+// MARK: - CategoryViewModel
+class CategoryViewModel:ObservableObject {
+    var model = HorizontalScrollerModel()
 }
 // MARK: - CategoryHosting
 class CategoryViewHostingController:UIHostingController<CategoryView> {
